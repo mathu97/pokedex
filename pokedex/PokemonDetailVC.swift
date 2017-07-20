f26ee95872001d22dedbe6a65998366241f1d4d1
Mathusan Selvarajah <mathusans52@gmail.com>
2017-07-19 23:08:18 -0400

    Adding initial segue class

---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by Mathusan Selvarajah on 2017-07-19.
+//  Copyright © 2017 Mathusan Selvarajah. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
